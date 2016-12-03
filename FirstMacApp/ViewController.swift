379bc369b2ae1d9997af95cb78e5ccbd1841c6b6
AbelSu <abelsu1993@outlook.com>
2016-12-03 13:14:32 +0800

    Initial Commit

---
+++ FirstMacApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FirstMacApp
+//
+//  Created by AbelSu on 2016/12/3.
+//  Copyright © 2016年 AbelSu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
